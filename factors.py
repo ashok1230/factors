9ca3b544169b4fe72c2047dc72be561efaac07a7
ashok1230 <gadiashok0410@gmail.com>
2013-03-23 11:20:30 +0530

    Create factors.py

---
+++ factors.py
@@ -0,0 +1,13 @@
+class factor:
+    def __init__(self,number):
+      self.i=number
+	    i=2
+	    while self.i>=i:
+	      if self.i%i==0:
+		      print i,
+		      self.i/=i
+		      i=2
+		      continue
+	      i+=1
+
+factor(100)
